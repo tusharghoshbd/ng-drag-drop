7a88f2176c789cd637ece30491934dd52fe9ad4c
Tushar <tusharghosh09006@gmail.com>
2020-03-14 02:10:40 +0600

    -- adding Input and output properties -- added action -- update the html code

--- projects/drag-drop-test/src/app/app.component.ts
+++ projects/drag-drop-test/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'drag-drop-test';
+  public documentList: any = [];
+  public documentTypeList: any = [];
+  ngOnInit() {
+        for(let i=1; i<=5; i++){
+            this.documentList.push({ "id": i, "content": 'file_name_'+i+'.pdf', effectAllowed: "copyMove", disable: false, handle: false, show: true });
+            //this.documentTypeListBK.push({ "id": "doctype_" + i, "content": "", "type": "Label "+i,  max: 1, effectAllowed: "move", disable: false, handle: false, empty: true });
+            this.documentTypeList.push({ "id":  i, "content": "", "type": "Label "+i,  max: 1, effectAllowed: "move", disable: false, handle: false, empty: true });
+        }
+    }
+
+    onDraggableData(itemObj:any){
+        console.log(itemObj);
+    }
+    onLeftDraggableDrop(leftItemArr:any){
+        console.log("onLeftDraggableDrop");
+        console.log(leftItemArr);
+    }
+    onRightDraggableDrop(rightItemArr:any){
+        console.log("onRightDraggableDrop");
+        console.log(rightItemArr);
+    }
 }
--- projects/drag-drop/src/lib/drag-drop.component.ts
+++ projects/drag-drop/src/lib/drag-drop.component.ts
@@ -1,30 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output ,EventEmitter   } from '@angular/core';
 
 @Component({
     selector: 'ng-drag-drop',
     templateUrl: './drag-drop.component.html',
-    styleUrls: ['./drag-drop.component.css']
+    styleUrls: [
+        './drag-drop.component.scss'
+    ]
 })
 export class DragDropComponent implements OnInit {
-    public documentList: any = [];
-    public documentTypeList: any = [];
-    public documentTypeListBK: any = [];
+    
+    @Input() public documentList: any = [];
+    @Input() public documentTypeList: any = [];
     public currentDraggableEvent: DragEvent;
+    @Output() onDraggable: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onLeftDraggableDrop: EventEmitter<any> = new EventEmitter<any>();
+    @Output() onRightDraggableDrop: EventEmitter<any> = new EventEmitter<any>();
+
 
     constructor() {
     }
 
     ngOnInit() {
-        for(let i=1; i<=5; i++){
-            this.documentList.push({ "id": i, "content": 'file_name_'+i+'.pdf', effectAllowed: "copyMove", disable: false, handle: false, show: true });
-            this.documentTypeListBK.push({ "id": "doctype_" + i, "content": "", "type": "Label "+i,  max: 1, effectAllowed: "move", disable: false, handle: false, empty: true });
-            this.documentTypeList.push({ "id": "doctype_" + i, "content": "", "type": "Label "+i,  max: 1, effectAllowed: "move", disable: false, handle: false, empty: true });
-        }
-        
+        // for(let i=1; i<=5; i++){
+        //     this.documentList.push({ "id": i, "content": 'file_name_'+i+'.pdf', effectAllowed: "copyMove", disable: false, handle: false, show: true });
+        //     this.documentTypeListBK.push({ "id": "doctype_" + i, "content": "", "type": "Label "+i,  max: 1, effectAllowed: "move", disable: false, handle: false, empty: true });
+        //     this.documentTypeList.push({ "id": "doctype_" + i, "content": "", "type": "Label "+i,  max: 1, effectAllowed: "move", disable: false, handle: false, empty: true });
+        // }
     }
 
     onDraggableStart(item: any) {
         this.currentDraggableEvent = item;
+        this.onDraggable.emit(item);
     }
 
     onLeftDraggableCopied(item: any) {
@@ -50,12 +56,14 @@ export class DragDropComponent implements OnInit {
         tempObj["handle"] = true;
         tempObj["show"] = true;
         this.documentList.splice(index, 0, tempObj);
+        this.onLeftDraggableDrop.emit(this.documentList);
     }
 
     onRightDrop(event: any, item: any) {
         let tempObj = JSON.parse(JSON.stringify(this.currentDraggableEvent));
         let index = this.documentTypeList.indexOf(item);
         this.documentTypeList[index].content = tempObj.content;
+        this.onRightDraggableDrop.emit(this.documentTypeList);
         //this.documentAssociationObj[this.employee] = JSON.parse(JSON.stringify(this.documentTypeList));
     }
 
